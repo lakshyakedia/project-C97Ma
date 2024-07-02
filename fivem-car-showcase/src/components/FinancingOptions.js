3480fee99b6b76ad050e2cce64574c249506d839
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 13:51:43 +0530

    Added: `fivem-car-showcase/src/components/FinancingOptions.js`

---
+++ fivem-car-showcase/src/components/FinancingOptions.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FinancingOptions = () => {
+  // Add logic for financing options here
+
+  return (
+    <div>
+      {/* Add JSX content for financing options */}
+    </div>
+  );
+};
+
+export default FinancingOptions;
