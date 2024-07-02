815a119eb71ce5c72a39c2f5f485dffdd8b8c4a4
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 13:51:27 +0530

    Added: `fivem-car-showcase/src/components/SocialMediaButtons.js`

---
+++ fivem-car-showcase/src/components/SocialMediaButtons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SocialMediaButtons = () => {
+    return (
+        <div>
+            {/* Code for social media sharing buttons */}
+        </div>
+    );
+};
+
+export default SocialMediaButtons;
