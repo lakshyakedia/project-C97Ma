eaa2ecb66c55d94955a7b8c0a192dbad1e0a06d4
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 13:51:36 +0530

    Added: `fivem-car-showcase/src/components/CarComparisonTool.js`

---
+++ fivem-car-showcase/src/components/CarComparisonTool.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CarComparisonTool = () => {
+  // Add your logic here
+
+  return (
+    <div>
+      {/* Add your JSX elements here */}
+    </div>
+  );
+};
+
+export default CarComparisonTool;
