ba6f9e86c83856ebff53a6f04862f75df0fe724c
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 13:52:08 +0530

    Added: `fivem-car-showcase/src/pages/CarDetails.js`

---
+++ fivem-car-showcase/src/pages/CarDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CarDetails = () => {
+    // Logic for fetching and displaying detailed car information
+    return (
+        <div>
+            <h1>Car Details Page</h1>
+            {/* Add code here to display detailed car information */}
+        </div>
+    );
+};
+
+export default CarDetails;
