f52eedcca1be7fbb532722b6503e7cefe2e83c0d
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-02 13:51:25 +0530

    Added: `fivem-car-showcase/src/components/SearchEngineOptimization.js`

---
+++ fivem-car-showcase/src/components/SearchEngineOptimization.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SearchEngineOptimization = () => {
+    return (
+        <div>
+            <h1>Search Engine Optimization</h1>
+            <p>Implementing SEO strategies to improve website visibility.</p>
+        </div>
+    );
+};
+
+export default SearchEngineOptimization;
